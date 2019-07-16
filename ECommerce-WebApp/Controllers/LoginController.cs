a39778977784aa01bfb35b3797cbaf803727bd8c
fatihcankurtaran <fatihcankurtarn@gmail.com>
2019-07-16 21:56:01 +0300

    api request added

--- ECommerce-WebApp/Controllers/LoginController.cs
+++ ECommerce-WebApp/Controllers/LoginController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ECommerce_WebApp.Dtos;
 using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ECommerce_WebApp.Controllers
 {
@@ -20,10 +22,18 @@ namespace ECommerce_WebApp.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult RegisterUser(UserDto userDto)
+        public async Task<IActionResult> RegisterUser(UserDto userDto)
         {
             if(ModelState.IsValid)
             {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:2622");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiVXNlciIsIm5iZiI6MTU2MzExNTMwNSwiZXhwIjoxNTYzNzIwMTA1LCJpYXQiOjE1NjMxMTUzMDUsImlzcyI6ImZhdGloY2Fua3VydGFyYW4uY29tIiwiYXVkIjoiZmF0aWgifQ.nTeglVKitMumNWGZ5NQeGruz8Hos8tTHpiWVwZwrRYs");
+                var result = await client.GetStringAsync("/users/getall");
+                
+                
+                Console.WriteLine(result);
+
                 HttpContext.Session.Set("SessionId", Encoding.UTF8.GetBytes(HttpContext.Session.Id));
                 return RedirectToAction(controllerName: "Home", actionName: "Index");
 
--- ECommerce-WebApp/Program.cs
+++ ECommerce-WebApp/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,7 +16,7 @@ namespace ECommerce_WebApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+           CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
